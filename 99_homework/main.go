f8fd75d39c57d18b1f838dd01aaf1cdeaf6c483f
Oleksandr Podustov <oleksandr_podustov@epam.com>
2017-07-07 20:21:47 +0300

    solved first week hometask

---
+++ 99_homework/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"strconv"
+)
+
+func ReturnInt() int { return 1 }
+
+func ReturnFloat() float32 { return 1.1 }
+
+func ReturnIntArray() [3]int {
+	return [3]int{1, 3, 4}
+}
+
+func ReturnIntSlice() []int {
+	return []int{1, 2, 3}
+}
+
+func IntSliceToString(in []int) string {
+	var res string
+	for _, v := range in {
+		res += strconv.Itoa(v)
+	}
+	return res
+}
+
+func MergeSlices(inF []float32, inI []int32) []int {
+	var res []int = make([]int, len(inF))
+	for i, v := range inF {
+		res[i] = int(v)
+	}
+	for _, v := range inI {
+		res = append(res, int(v))
+	}
+
+	return res
+}
+
+func GetMapValuesSortedByKey(in map[int]string) []string {
+	var res []string
+	var rawKeys []int
+	for i, _ := range in {
+		rawKeys = append(rawKeys, i)
+	}
+
+	var ii int = 1
+
+	for i := 0; i < len(rawKeys); i++ {
+		if rawKeys[ii] > rawKeys[i] {
+			rawKeys[ii], rawKeys[i] = rawKeys[i], rawKeys[ii]
+			i = 0
+		}
+		ii = i
+	}
+
+	for i := 0; i < len(in); i++ {
+		key := rawKeys[i]
+		res = append(res, in[key])
+	}
+
+	return res
+}
