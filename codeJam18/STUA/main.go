03a8d62c87a402c0890ff851afd587ac1cac377f
Oleksandr Podustov <kydoter@gmail.com>
2018-04-07 16:12:50 +0300

    wrote some tests
    Signed-off-by: opodu <opodu@softserveinc.com>
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- codeJam18/STUA/main.go
+++ codeJam18/STUA/main.go
@@ -12,7 +12,6 @@ import (
 
 func main() {
 	var (
-		//testCasesAmount int
 		caseNumber int
 	)
 
@@ -41,36 +40,66 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			result := calc(uint(maxWithstand), slsl[1])
-			fmt.Println("Case #", caseNumber, ":", result)
+			result := calc(uint(maxWithstand), &slsl[1])
+			fmt.Printf("Case #%v: %v\n", caseNumber, result)
 		}
 		if len(slsl) > 2 {
 			log.Fatal("\t len(slsl) > 2 - unexxpected string length")
 		}
-		//fmt.Printf("%#v \n", slsl)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("1<<2: %v \n", 1<<1)
 }
 
-func calc(maxWithstand uint, input []byte) string {
+func calc(maxWithstand uint, input *[]byte) string {
 	var (
 		minTotalDamage uint
-		damage         uint = 1
 	)
-	//fmt.Println("input b", input)
-	input = bytes.TrimRight(input, `C`)
-	//fmt.Println("input a", input)
-	if len(input) == 0 {
-		//fmt.Println("\t\t\trobot will never shoot")
+	*input = bytes.TrimRight(*input, `C`)
+	if len(*input) == 0 {
+		//fmt.Println("\t\t\t robot will never shoot")
 		return "0"
 	}
 
-	fmt.Printf("\t maxWithstand:%v [%q]\n", maxWithstand, input)
-	for i, v := range input {
+	minTotalDamage = countDamage(input)
+
+	if minTotalDamage <= maxWithstand {
+		//fmt.Println("\t\t minTotalDamage <= maxWithstand. we don't need to swap")
+		return "0"
+	}
+
+	var iterationsToDeactivate int
+	for {
+		if minTotalDamage <= maxWithstand {
+			return strconv.Itoa(iterationsToDeactivate)
+		}
+
+		combinationIndex := strings.LastIndex(string(*input), "CS")
+		if combinationIndex == -1 {
+			break
+		}
+
+		iterationsToDeactivate++
+		(*input)[combinationIndex], (*input)[combinationIndex+1] = (*input)[combinationIndex+1], (*input)[combinationIndex]
+		*input = bytes.TrimRight(*input, `C`)
+		//fmt.Println("\t new string(input):", string(input))
+		minTotalDamage = countDamage(input)
+	}
+
+	if minTotalDamage > maxWithstand {
+		//fmt.Println("\t IMPOSSIBLE input:", string(input))
+		return "IMPOSSIBLE"
+	}
+
+	return strconv.Itoa(iterationsToDeactivate)
+}
+
+func countDamage(input *[]byte) uint {
+	var damage uint = 1
+	var minTotalDamage uint
+	for i, v := range *input {
 		switch v {
 		case 'C':
 			damage = damage * 2
@@ -82,40 +111,6 @@ func calc(maxWithstand uint, input []byte) string {
 			fmt.Println("\t default case: ", i, v)
 		}
 	}
-	//fmt.Println("Min total damage without swap: ", minTotalDamage)
 
-	if minTotalDamage <= maxWithstand {
-		//fmt.Println("\t\t minTotalDamage <= maxWithstand. we don't need to swap")
-		return "0"
-	}
-
-	// swap
-	var amountOfiterationsToDeactivate int
-	for {
-		fmt.Println("\t minTotalDamage, damage", minTotalDamage, damage)
-		if minTotalDamage <= maxWithstand {
-			//fmt.Println("\t\t minTotalDamage <= maxWithstand. stop swapping. minTotalDamage:", strconv.Itoa(int(minTotalDamage)))
-			return strconv.Itoa(amountOfiterationsToDeactivate)
-		}
-
-		combinationIndex := strings.LastIndex(string(input), "CS")
-		if combinationIndex == -1 {
-			break
-		}
-
-		//fmt.Println("\t combinationIndex", combinationIndex)
-		amountOfiterationsToDeactivate++
-		input[combinationIndex], input[combinationIndex+1] = input[combinationIndex+1], input[combinationIndex]
-		fmt.Println("\t new string(input):", string(input))
-
-		damage -= damage / 2
-		minTotalDamage -= damage
-	}
-
-	if minTotalDamage > maxWithstand {
-		fmt.Println("IMPOSSIBLE input:", string(input))
-		return "IMPOSSIBLE"
-	}
-
-	return strconv.Itoa(amountOfiterationsToDeactivate)
+	return minTotalDamage
 }
