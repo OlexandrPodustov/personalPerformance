dad70dfc2bebeb324dce1b07f482d888851ed540
Oleksandr Podustov <kydoter@gmail.com>
2020-04-06 22:28:11 +0300

    added 2018 r1A begin of solution
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

---
+++ gcj/2018/r1A/waffleChoppers/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var testCasesAmount int
+	if _, err := fmt.Scan(&testCasesAmount); err != nil {
+		panic("parse testCasesAmount:" + err.Error())
+	}
+
+	for testCaseNumber := 1; testCaseNumber <= testCasesAmount; testCaseNumber++ {
+		var rows, columns, horizontal, vertical int
+		if _, err := fmt.Scan(&rows, &columns, &horizontal, &vertical); err != nil {
+			panic("&rows, &columns, &horizontal, &vertical:" + err.Error())
+		}
+
+		var ca int
+		for i := 0; i < rows; i++ {
+			var row string
+			if _, err := fmt.Scan(&row); err != nil {
+				fmt.Printf("scan row: %v", err)
+				os.Exit(1)
+			}
+			ca += strings.Count(row, "@")
+		}
+
+		fmt.Println("ca total", ca, "parts", rows, columns, horizontal, vertical, 4)
+	}
+}
+
+// calculate how many parts will be after division
+// divide total amount of chocolate chips by total parts
+// check if this result is possible for each combination
