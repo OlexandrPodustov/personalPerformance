cab181dece68061df52eb2541b8e8a3bcf9ce779
Oleksandr Podustov <kydoter@gmail.com>
2023-01-17 09:48:35 +0200

    happy number done
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

---
+++ educative/go/happyNumber/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func Test_happyNumber(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "1",
+			args: args{
+				num: 1,
+			},
+			want: true,
+		},
+		{
+			name: "28",
+			args: args{
+				num: 28,
+			},
+			want: true,
+		},
+		{
+			name: "2147483646",
+			args: args{
+				num: 2147483646,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := happyNumber(tt.args.num); got != tt.want {
+				t.Errorf("happyNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
