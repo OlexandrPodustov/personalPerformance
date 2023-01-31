61fc14c5b0fd0823a11872a21203e4453b735d5c
Oleksandr Podustov <kydoter@gmail.com>
2023-01-31 15:05:18 +0200

    educative: findMaxSlidingWindow solved not optimal, will revisit and implement properly later
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- educative/go/findMaximuminSlidingWindow/main.go
+++ educative/go/findMaximuminSlidingWindow/main.go
@@ -3,5 +3,28 @@ package main
 func findMaxSlidingWindow(nums []int, windowSize int) []int {
 	result := make([]int, 0)
 
+	deq := NewDeque()
+	for _, v := range nums {
+		if deq.Len() < windowSize {
+			deq.PushBack(v)
+			continue
+		}
+		result = append(result, deq.currentTop())
+		deq.PopFront()
+		deq.PushBack(v)
+	}
+	result = append(result, deq.currentTop())
+
 	return result
+}
+
+func (s *Deque) currentTop() int {
+	maxVal := -99_999
+	for _, v := range s.items {
+		if v > maxVal {
+			maxVal = v
+		}
+	}
+
+	return maxVal
 }
