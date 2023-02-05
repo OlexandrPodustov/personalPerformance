ff2c5cb67e2f8f926e0c930464c62694bea9974f
Oleksandr Podustov <kydoter@gmail.com>
2023-02-05 16:24:09 +0200

    added tree task

---
+++ leetcode/101SymmetricTree/main.go
@@ -0,0 +1,12 @@
+package main
+
+// TreeNode - definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return true
+}
