2d12d3769ff9c02e68b1959101824bbb6b31ee9b
Oleksandr Podustov <kydoter@gmail.com>
2023-02-08 07:58:33 +0200

    minimum window subsequence

---
+++ leetcode/1342NumberofStepstoReduceaNumbertoZero/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func Test_numberOfSteps(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				num: 14,
+			},
+			want: 6,
+		},
+
+		{
+			name: "",
+			args: args{
+				num: 8,
+			},
+			want: 4,
+		},
+
+		{
+			name: "",
+			args: args{
+				num: 123,
+			},
+			want: 12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfSteps(tt.args.num); got != tt.want {
+				t.Errorf("numberOfSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
