9030cd24feeb6a6de8da21d26729863d7c664fb8
Oleksandr Podustov <kydoter@gmail.com>
2023-05-25 15:54:46 +0300

    solved two sum
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- leetcode/1.TwoSum/main.go
+++ leetcode/1.TwoSum/main.go
@@ -1,5 +1,21 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	return nil
+	numberIndex := make(map[int]int, len(nums))
+	for i, v := range nums {
+		numberIndex[v] = i
+	}
+
+	result := make([]int, 0, 2)
+	for i, v := range nums {
+		j, ok := numberIndex[target-v]
+		if ok && i != j {
+			result = append(result, i)
+			result = append(result, j)
+
+			break
+		}
+	}
+
+	return result
 }
