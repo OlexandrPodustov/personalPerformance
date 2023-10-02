87b9a68283fa7ac7d1819dabf08b2f6898605c90
Oleksandr Podustov <kydoter@gmail.com>
2023-10-02 22:23:12 +0300

    added leetcode 283.MoveZeroes
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

---
+++ leetcode/283.MoveZeroes/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				nums: []int{0, 1, 0, 3, 12},
+			},
+			want: []int{1, 3, 12, 0, 0},
+		},
+		{
+			name: "2",
+			args: args{
+				nums: []int{0},
+			},
+			want: []int{0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+			require.Equal(t, tt.want, tt.args.nums)
+		})
+	}
+}
