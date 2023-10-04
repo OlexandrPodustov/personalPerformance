9f7bca4f7d05f5377dc470413d03b492e7070dd1
Oleksandr Podustov <kydoter@gmail.com>
2023-10-04 20:32:29 +0300

    bandwith throttle draft
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- diff/scylla/task2/main.go
+++ diff/scylla/task2/main.go
@@ -20,9 +20,68 @@
 // * example: limit x kB/s means all connections can cumulatively consume at most x kB in a given second
 
 // ** example: limit y kB/s means each and every connection can consume at most y kB independently from each other in a given second
-
 package main
 
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/olexandrPodustov/diff/scylla/task2/bandwidththrottle"
+	"golang.org/x/time/rate"
+)
+
 func main() {
-	println(1)
+	// Create a BandwidthThrottle with a global rate of 1024 bytes per second.
+	bt := bandwidththrottle.NewBandwidthThrottle(rate.Limit(1024))
+
+	// Create a TCP server.
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("failed starting the listener:", err)
+		return
+	}
+	defer listener.Close()
+
+	// Accept and handle incoming connections.
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("failed accepting the connection:", err)
+			continue
+		}
+
+		// Set a connection-specific bandwidth rate.
+		bt.SetConnectionRate(conn, rate.Limit(512)) // 512 bytes per second
+
+		// Wrap the connection with bandwidth throttling.
+		throttledConn := bt.WrapConnection(conn)
+
+		// Handle the connection in a goroutine.
+		go handleConnection(throttledConn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	data := make([]byte, 1024)
+	for {
+		n, err := conn.Read(data)
+		if err != nil {
+			fmt.Println("failed reading the data:", string(data), err)
+			return
+		}
+
+		// Simulate data processing.
+		processData(data[:n])
+	}
+}
+
+func processData(data []byte) {
+	// Simulate data processing.
+	fmt.Printf("Received %d bytes\n", len(data))
+
+	// Introduce some delay to see throttling in action.
+	time.Sleep(time.Millisecond * 100)
 }
