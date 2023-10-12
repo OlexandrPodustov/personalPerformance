750d5791a9758975db54528df15a37b0a3add634
Oleksandr Podustov <kydoter@gmail.com>
2023-10-12 23:18:15 +0300

    leetcode 20 valid parentheses

---
+++ leetcode/20.ValidParentheses/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"strings"
+)
+
+func isValid(s string) bool {
+	r := strings.NewReplacer("()", "", "[]", "", "{}", "")
+
+	oldLen := 0
+	for oldLen != len(s) {
+		oldLen = len(s)
+		s = r.Replace(s)
+	}
+
+	return s == ""
+}
