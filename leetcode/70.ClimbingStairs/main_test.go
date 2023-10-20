b91728f1ff90df406de185b96a4425a936984888
Oleksandr Podustov <kydoter@gmail.com>
2023-10-20 18:46:03 +0300

    leetcode climbStairs
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- leetcode/70.ClimbingStairs/main_test.go
+++ leetcode/70.ClimbingStairs/main_test.go
@@ -18,6 +18,41 @@ func Test_climbStairs(t *testing.T) {
 			},
 			want: 2,
 		},
+		{
+			name: "3",
+			args: args{
+				n: 3,
+			},
+			want: 3,
+		},
+		{
+			name: "4",
+			args: args{
+				n: 4,
+			},
+			want: 5,
+		},
+		{
+			name: "5",
+			args: args{
+				n: 5,
+			},
+			want: 8,
+		},
+		{
+			name: "6",
+			args: args{
+				n: 6,
+			},
+			want: 13,
+		},
+		{
+			name: "7",
+			args: args{
+				n: 7,
+			},
+			want: 21,
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
