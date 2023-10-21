7439159300d945df2d487dccf040fb3cd7c72b2a
Oleksandr Podustov <kydoter@gmail.com>
2023-10-21 18:33:15 +0300

    leetcode 110.BalancedBinaryTree almost working

--- leetcode/110.BalancedBinaryTree/main.go
+++ leetcode/110.BalancedBinaryTree/main.go
@@ -8,6 +8,48 @@ type TreeNode struct {
 }
 
 func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	lh := findHeight(root.Left)
+	rh := findHeight(root.Right)
 
-	return false
+	// fmt.Println("main", lh, rh, abs(lh-rh))
+
+	if abs(lh-rh) > 1 {
+		return false
+	}
+
+	return true
+}
+
+func findHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	height := 1
+	lh, rh := 0, 0
+	if root.Left != nil {
+		lh = findHeight(root.Left)
+	}
+	if root.Right != nil {
+		rh = findHeight(root.Right)
+	}
+
+	if lh > rh {
+		height += lh
+	} else {
+		height += rh
+	}
+	// fmt.Println("findHeight", root.Val, height, lh, rh)
+
+	return height
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -1 * n
+	}
+	return n
 }
--- leetcode/110.BalancedBinaryTree/main_test.go
+++ leetcode/110.BalancedBinaryTree/main_test.go
@@ -1,1 +1,107 @@
 package main
+
+import (
+	"testing"
+)
+
+func Test_isBalanced(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "3,9,20,null,null,15,7",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 20,
+						Left: &TreeNode{
+							Val: 15,
+						},
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "1,2,2,3,3,null,null,4,4",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+							Left: &TreeNode{
+								Val: 4,
+							},
+							Right: &TreeNode{
+								Val: 4,
+							},
+						},
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "[]",
+			args: args{
+				root: nil,
+			},
+			want: true,
+		},
+		{
+			name: "[1,2,2,3,null,null,3,4,null,null,4]",
+			args: args{
+				root: &TreeNode{
+					Val:   1,
+					Left:  &TreeNode{Val: 2, Left: &TreeNode{Val: 3, Left: &TreeNode{Val: 4}}},
+					Right: &TreeNode{Val: 2, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "1,null,2,null,3",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Right: &TreeNode{
+						Val: 2,
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := isBalanced(tt.args.root); got != tt.want {
+				t.Errorf("isBalanced(%v) = %v, want %v", tt.args.root, got, tt.want)
+			}
+		})
+	}
+}
