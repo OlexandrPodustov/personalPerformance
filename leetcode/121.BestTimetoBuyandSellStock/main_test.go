d82410b2c44936aa1c0fd215b4d9d9823a5f9dc1
Oleksandr Podustov <kydoter@gmail.com>
2023-10-22 16:56:48 +0300

    leetcode 121 best time to buy and sell - solved

--- leetcode/121.BestTimetoBuyandSellStock/main_test.go
+++ leetcode/121.BestTimetoBuyandSellStock/main_test.go
@@ -1,1 +1,38 @@
 package main
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "7,1,5,3,6,4",
+			args: args{
+				prices: []int{7, 1, 5, 3, 6, 4},
+			},
+			want: 5,
+		},
+		{
+			name: "7,6,4,3,1",
+			args: args{
+				prices: []int{7, 6, 4, 3, 1},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit(%v) = %v, want %v", tt.args.prices, got, tt.want)
+			}
+		})
+	}
+}
