dcdb5c6fed503933171a433a3de93578f4565e48
Oleksandr Podustov <kydoter@gmail.com>
2023-10-22 16:56:48 +0300

    leetcode 121 best time to buy and sell - solved
    Signed-off-by: Oleksandr Podustov <kydoter@gmail.com>

--- leetcode/121.BestTimetoBuyandSellStock/main.go
+++ leetcode/121.BestTimetoBuyandSellStock/main.go
@@ -1,5 +1,17 @@
 package main
 
 func maxProfit(prices []int) int {
-	return 0
+	localMin := 10000
+
+	res := 0
+	for _, v := range prices {
+		if localMin > v {
+			localMin = v
+		}
+		if margin := v - localMin; margin > res {
+			res = margin
+		}
+	}
+
+	return res
 }
