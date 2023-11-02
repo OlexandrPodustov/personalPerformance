0710bd0e84df8d61eeb7cedd9df523d30a09b158
Oleksandr Podustov <kydoter@gmail.com>
2023-11-02 22:24:40 +0200

    leetcode 844.BackspaceStringCompare solved with all tests

--- leetcode/844.BackspaceStringCompare/main.go
+++ leetcode/844.BackspaceStringCompare/main.go
@@ -12,15 +12,21 @@ func backspaceCompare(s string, t string) bool {
 func replaceBackspaces(s string) string {
 	i := 0
 	for len(s) >= 0 && i < len(s) {
-		if s[i] == back {
-
+		if s[i] == back && i > 0 {
 			s = s[:i-1] + s[i+1:]
-
 			i--
 			continue
 		}
 		i++
 	}
+	// fmt.Println("int s", s)
+
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == back {
+			s = s[:i] + s[i+1:]
+		}
+	}
+
 	// fmt.Println("new s", s)
 
 	return s
