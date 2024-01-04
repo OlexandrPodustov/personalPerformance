ad8bcd1ba912d47fba89a9351b2f8a63b34c06f3
Oleksandr Podustov <kydoter@gmail.com>
2024-01-04 23:40:31 +0200

    leetcode 2870.MinimumNumberofOperationstoMakeArrayEmpty

---
+++ leetcode/2870.MinimumNumberofOperationstoMakeArrayEmpty/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func Test_minOperations(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				nums: []int{2, 3, 3, 2, 2, 4, 2, 3, 4},
+			},
+			want: 4,
+		},
+		{
+			name: "2",
+			args: args{
+				nums: []int{2, 1, 2, 2, 3, 3},
+			},
+			want: -1,
+		},
+		{
+			name: "3",
+			args: args{
+				nums: []int{14, 12, 14, 14, 12, 14, 14, 12, 12, 12, 12, 14, 14, 12, 14, 14, 14, 12, 12},
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minOperations(tt.args.nums); got != tt.want {
+				t.Errorf("minOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
