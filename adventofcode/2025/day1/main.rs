7c3165992504433d866d844189cbd378ad68c32b
Oleksandr Podustov <kydoter@gmail.com>
2025-12-04 10:13:05 +0200

    day 1 solved

---
+++ adventofcode/2025/day1/main.rs
@@ -0,0 +1,39 @@
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let mut result = 0;
+    let mut points_to = 50;
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input_line = line?;
+        let (direction, distance) = input_line.split_at(1);
+        let distance_int = distance.parse::<i32>().unwrap();
+        let distance_mod = distance_int % 100;
+
+        println!(
+            "direction: {}, distance: {}, distance modulo: {}",
+            direction, distance_int, distance_mod
+        );
+
+        if direction == "L" {
+            points_to -= distance_mod;
+        } else {
+            points_to += distance_mod;
+        }
+        if points_to < 0 {
+            points_to += 100;
+        } else if points_to >= 100 {
+            points_to -= 100;
+        }
+        println!("  points_to: {}", points_to);
+
+        if points_to == 0 {
+            result += 1;
+        }
+    }
+
+    println!("result: {}", result);
+
+    Ok(())
+}
