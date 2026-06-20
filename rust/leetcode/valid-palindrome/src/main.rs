00f542793d66bf7bf4f6ca51f479f70337c7920d
Oleksandr Podustov <kydoter@gmail.com>
2026-06-20 16:40:57 +0300

    Implement palindrome checking logic and add unit tests

--- rust/leetcode/valid-palindrome/src/main.rs
+++ rust/leetcode/valid-palindrome/src/main.rs
@@ -6,6 +6,33 @@ impl Solution {
     #[must_use]
     pub fn is_palindrome(_s: String) -> bool {
         false
+    #[must_use]
+    pub fn is_palindrome(s: String) -> bool {
+        let vc: Vec<char> = s
+            .chars()
+            .filter(|&s| s.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+
+        if vc.is_empty() {
+            return true;
+        }
+
+        let end = vc.len();
+        let (mut s, mut e) = (0, end - 1);
+        loop {
+            if s >= e {
+                break;
+            }
+
+            if vc[s] != vc[e] {
+                return false;
+            }
+            s += 1;
+            e -= 1;
+        }
+
+        true
     }
 }
 fn main() {
@@ -14,6 +41,17 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        assert_eq!(
+            Solution::is_palindrome("A man, a plan, a canal: Panama".to_owned()),
+            true
+        );
+        assert_eq!(Solution::is_palindrome("race a car".to_owned()), false);
+        assert_eq!(Solution::is_palindrome(" ".to_owned()), true);
+        assert_eq!(Solution::is_palindrome("ab cd".to_owned()), false);
+        assert_eq!(Solution::is_palindrome("ab cd".to_owned()), false);
+    }
 }
